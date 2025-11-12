113ee15e1167e7905278edae515108b10cb0e6f0
Ariunjargal Ochirpurev <ariunjargal.ochirpurev@gmail.com>
2025-11-12 23:25:16 +0900

    adding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div> he;;    </div>
+    <div> he;;
+      <p className="text-3xl">
+        Hello 
+        </p>    </div>
   );
 }
 //qwertf
